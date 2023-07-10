086e67d28e802bbf524e6c22d6907c0672a10aa8
mizar <mizarzulfaz@gmail.com>
2023-07-11 00:50:04 +0700

    chat history update_v1

--- discord-jipity.py
+++ discord-jipity.py
@@ -2,6 +2,7 @@ import discord
 from privateToken import Discord_api_K3y
 from privateToken import openAI_api_k3y
 import requests
+import json
 
 
 def model_list():
@@ -10,6 +11,13 @@ def model_list():
 
 
 model = model_list()[1]
+max_messages = 8
+conversation = [
+                {"role": "system", "content": "You answer questions based on a provided context."}
+                # {"role": "assistant", "content": ""}
+                # {"role": "user", "content": ""}
+                # {"role": "user", "content": str(message.content)}
+                ]
 
 # Set the endpoint URL
 url = "https://api.openai.com/v1/chat/completions"
@@ -19,7 +27,6 @@ headers_request = {
     "Content-Type": "application/json",
     "Authorization": f"Bearer {openAI_api_k3y()}"
 }
-
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -33,18 +40,35 @@ class MyClient(discord.Client):
         # Set the payload (JSON data)
         payload = {
             "model": model,
-            "messages": [{"role": "user", "content": str(message.content)}],
+            "messages": conversation,
             "n": 1
+            # "max_tokens" : not set (integer)
         }
-
+        
+        def update_conversation_history(conversation, new_message):
+            conversation.append(new_message)
+            if len(conversation) > max_messages:
+                for delete in range(1,3):
+                    conversation.pop(delete)
+                # conversation.pop(1)
+                # conversation.pop(2)
+                    
+        new_message = {"role": "user", "content": str(message.content)}
+        update_conversation_history(conversation, new_message)
+                
         # Send the POST request
         response = requests.post(url, headers=headers_request, json=payload)
-        # Get the response JSON data
+        
+        # Get the response JSON data, arrange JSON data with JSON Module
         json_data = response.json()
+        print(json.dumps(json_data, indent=4))
+        print(len(conversation))
 
         chatjipiti_answer = json_data["choices"][0]["message"]["content"]
         await message.channel.send(chatjipiti_answer)
-
+        conversation.append({"role": "assistant", "content": chatjipiti_answer})
+        
+        await message.channel.send(f'Total tokens : {json_data["usage"]["total_tokens"]}')
 
 intents = discord.Intents.default()
 intents.message_content = True
